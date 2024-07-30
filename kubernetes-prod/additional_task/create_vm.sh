93668e99891860378056aa78ac90f3deca4c676d
RFF-13 <pmail@disroot.org>
2024-07-30 20:11:43 +0300

    Create VMs

---
+++ kubernetes-prod/additional_task/create_vm.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+export FOLDER=$(yc config get folder-id)
+export ZONE=ru-central1-a
+export SUBNET=default-ru-central1-a
+
+# Create masters
+for i in 1 2 3
+do
+  yc compute instance create \
+    --folder-id $FOLDER \
+    --name k8s-node-master-$1 \
+    --description "k8s cluster" \
+    --hostname master-$1 \
+    --zone $ZONE \
+    --platform standard-v2 \
+    --create-boot-disk image-family=ubuntu-2404-lts-oslogin,size=20,type=network-hdd \
+    --image-folder-id standard-images \
+    --memory 8 \
+    --cores 2 \
+    --core-fraction 20 \
+    --preemptible \
+    --network-settings type=standard \
+    --network-interface subnet-name=$SUBNET,nat-ip-version=ipv4 \
+    --ssh-key ~/.ssh/id_rsa.pub \
+    --metadata serial-port-enable=1 \
+    --async
+done
+
+# Create workers
+for i in 1 2
+do
+  yc compute instance create \
+    --folder-id $FOLDER \
+    --name k8s-node-worker-$i \
+    --description "k8s cluster" \
+    --hostname worker-$i \
+    --zone $ZONE \
+    --platform standard-v2 \
+    --create-boot-disk image-family=ubuntu-2404-lts-oslogin,size=20,type=network-hdd \
+    --image-folder-id standard-images \
+    --memory 8 \
+    --cores 2 \
+    --core-fraction 20 \
+    --preemptible \
+    --network-settings type=standard \
+    --network-interface subnet-name=$SUBNET,nat-ip-version=ipv4 \
+    --ssh-key ~/.ssh/id_rsa.pub \
+    --metadata serial-port-enable=1 \
+    --async
+done
